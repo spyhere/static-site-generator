0fa53b94f96ed433b124cc5edaa7742b00bf874e
spyhere <gekav3@mail.ru>
2025-11-27 22:55:25 +0200

    Create TestNode class with tests

---
+++ src/test_textnode.py
@@ -0,0 +1,26 @@
+import unittest
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def should_not_eq(self):
+        node = TextNode("Node 1", TextType.PLAIN)
+        node2 = TextNode("Node 2", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def should_accept_url(self):
+        url = "http://localhost:3000"
+        node_without_url = TextNode("Node without url", TextType.PLAIN)
+        node_with_url = TextNode("Node with url", TextType.PLAIN, url)
+        self.assertNotIn(url, str(node_without_url))
+        self.assertIn(url, str(node_with_url))
+
+
+if __name__ == "__main__":
+    unittest.main()
+
---
+++ src/textnode.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+class TextType(Enum):
+    PLAIN = "plain"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+
+class TextNode():
+    def __init__(self, text: str, text_type: TextType, url: str|None = None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, value: object) -> bool:
+        if isinstance(value, TextNode):
+            return (self.text == value.text) and (self.text_type == value.text_type) and (self.url == value.url)
+        return False
+
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
