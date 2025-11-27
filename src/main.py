ba818464d7a2df9227d53671ae9dea305e3c147e
spyhere <gekav3@mail.ru>
2025-11-27 22:56:01 +0200

    Add `main.py`

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from textnode import TextNode, TextType
+
+
+def main():
+    node = TextNode("test text", TextType.PLAIN, "https://google.com")
+    print(node)
+
+
+if __name__ == "__main__":
+    main()
+
