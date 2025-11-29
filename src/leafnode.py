44e74911ebaaadc0ea6ac57233be2bf06be8ec1a
spyhere <gekav3@mail.ru>
2025-11-29 15:20:34 +0200

    Add header and quote support for LeafNode

--- src/leafnode.py
+++ src/leafnode.py
@@ -11,6 +11,18 @@ class LeafNode(HTMLNode):
         if not self.tag:
             return self.value
         match self.tag:
+            case "h1":
+                return f"<h1>{self.value}</h1>"
+            case "h2":
+                return f"<h2>{self.value}</h2>"
+            case "h3":
+                return f"<h3>{self.value}</h3>"
+            case "h4":
+                return f"<h4>{self.value}</h4>"
+            case "h5":
+                return f"<h5>{self.value}</h5>"
+            case "h6":
+                return f"<h6>{self.value}</h6>"
             case "p":
                 return f"<p>{self.value}</p>"
             case "a":
@@ -25,6 +37,8 @@ class LeafNode(HTMLNode):
                 return f"<div>{self.value}</div>"
             case "code":
                 return f"<code>{self.value}</code>"
+            case "quote":
+                return f"<quote>{self.value}</quote>"
             case "img":
                 return f"<img{self.props_to_html()} />"
             case _:
