e2027556a5beb8d972b269257ac47941e75240ed
spyhere <gekav3@mail.ru>
2025-11-29 23:37:32 +0200

    Generate page and start the server on start

--- main.sh
+++ main.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
 
 python3 src/main.py
+cd public && echo "Starting server at http://localhost:8888"&& python3 -m http.server 8888
+
 
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from os_manipulations import remove_dir, create_dir, copy_dir_to_target
+from os_manipulations import generate_page, remove_dir, create_dir, copy_dir_to_target
 
 
 def main():
@@ -6,6 +6,7 @@ def main():
     create_dir("public")
     copy_dir_to_target("static", "public")
 
+    generate_page("content/index.md", "public/index.html", "template.html")
 
 if __name__ == "__main__":
     main()
