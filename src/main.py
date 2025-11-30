ad288fb397a0940b27f2c17b348e218cd969e376
spyhere <gekav3@mail.ru>
2025-11-30 12:42:30 +0200

    Make the program be able to run locally

--- src/main.py
+++ src/main.py
@@ -2,12 +2,16 @@ import sys
 from os_manipulations import generate_pages_recursive, remove_dir, create_dir, copy_dir_to_target
 
 
-def main(basepath: str = "/"):
-    remove_dir("docs")
-    create_dir("docs")
-    copy_dir_to_target("static", "docs")
-    generate_pages_recursive("content", "docs", "template.html", basepath)
+def main(basepath: str):
+    target_dir = "public" if basepath == "/" else "docs"
+    remove_dir(target_dir)
+    create_dir(target_dir)
+    copy_dir_to_target("static", target_dir)
+    generate_pages_recursive("content", target_dir, "template.html", basepath)
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    basepath = "/"
+    if 1 in sys.argv:
+        basepath = sys.argv[1]
+    main(basepath)
 
